d24943b2dccf8584a1ad2394deded3f0941be1bb
proin <proin@proinlab.com>
2021-02-05 07:14:02 +0000

    add import from module

--- framework/core/libbase.php
+++ framework/core/libbase.php
@@ -31,11 +31,22 @@ class LibBase
     public function lib($name)
     {
         $fn_name = basename($name);
+        $ns = explode("/", $name);
+        $ns = implode("\\", $ns);
 
-        $_path = $this->module_path("lib", $name);
-        if (file_exists($_path)) {
-            require_once($_path);
-            return new $fn_name;
+        // from module
+        if ($fn_name != $ns) {
+            $_path = $this->module_path("lib", $name);
+            if (file_exists($_path)) {
+                $ns = explode("/", $name);
+                if (count($ns[0]) > 0) {
+                    array_unshift($ns, "lib");
+                    array_unshift($ns, "framework");
+                }
+                $ns = "\\" . implode("\\", $ns);
+                require_once($_path);
+                return new $ns;
+            }
         }
 
         $_path = PATH_APP . "/lib/$name.php";
@@ -56,13 +67,25 @@ class LibBase
     public function util($name)
     {
         $fn_name = basename($name);
+        $ns = explode("/", $name);
+        $ns = implode("\\", $ns);
 
-        $_path = $this->module_path("util", $name);
-        if (file_exists($_path)) {
-            require_once($_path);
-            return new $fn_name;
+        // from module
+        if ($fn_name != $ns) {
+            $_path = $this->module_path("util", $name);
+            if (file_exists($_path)) {
+                $ns = explode("/", $name);
+                if (count($ns[0]) > 0) {
+                    array_unshift($ns, "util");
+                    array_unshift($ns, "framework");
+                }
+                $ns = "\\" . implode("\\", $ns);
+                require_once($_path);
+                return new $ns;
+            }
         }
 
+        // from app or core
         $_path = PATH_APP . "/util/$name.php";
         if (file_exists($_path)) {
             require_once($_path);
@@ -81,15 +104,26 @@ class LibBase
     public function model($name)
     {
         $fn_name = basename($name . "_model");
+        $ns = explode("/", $name);
+        $ns = implode("\\", $ns) . "_model";
 
-        $_path = $this->module_path("model", $name);
-        if (file_exists($_path)) {
-            require_once($_path);
-            return new $fn_name;
+        // from module
+        if ($fn_name != $ns) {
+            $_path = $this->module_path("model", $name);
+            if (file_exists($_path)) {
+                $ns = explode("/", $name);
+                if (count($ns[0]) > 0) {
+                    array_unshift($ns, "model");
+                    array_unshift($ns, "framework");
+                }
+                $ns = "\\" . implode("\\", $ns) . "_model";
+                require_once($_path);
+                return new $ns;
+            }
         }
 
+        // from app core
         $_path = PATH_APP . "/model/$name.php";
-
         if (file_exists($_path)) {
             require_once($_path);
             return new $fn_name;
